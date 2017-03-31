a3516dabf20b21c801e95c63c8d6d2e6a8051e7f
Milan van Dijck <miscoriadev@gmail.com>
2017-03-31 15:17:27 +0200

    Fixed another naming mistake

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ function checkPowerUsage(kWh, date){
                 datapointDate.setHours(parseInt(time[0]), parseInt(time[1]), parseInt(time[2]));
 
                 var datapoint = {
-                    kWh: parseInt(row.kwatts.toString()),
+                    kWh: parseInt(row.kWh.toString()),
                     time: datapointDate
                 };
                 calibrationDates.push(datapoint);
